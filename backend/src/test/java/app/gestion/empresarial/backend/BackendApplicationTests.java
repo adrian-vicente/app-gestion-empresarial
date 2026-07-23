5fec7917255c4d90c472b5a2d7881019b48ccf5e
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-07-23 12:54:04 +0200

    Subida estructura base del proyecto backend

---
+++ backend/src/test/java/app/gestion/empresarial/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.gestion.empresarial.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
