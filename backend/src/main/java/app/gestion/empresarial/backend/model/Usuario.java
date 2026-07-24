f6f6713eb226955d0de42b24419977fce2b281d3
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-07-24 18:05:08 +0200

    Creación base de modelos + modificación application.properties

---
+++ backend/src/main/java/app/gestion/empresarial/backend/model/Usuario.java
@@ -0,0 +1,25 @@
+package app.gestion.empresarial.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+} // class
