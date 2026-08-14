a5faf8c1f8afbd642ee0f98b5d8348404af0a270
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-08-14 13:47:01 +0200

    Creación de rutas accesibles para la aplicación.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  // Ruta que redirigirá al componente para iniciar sesión
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: '*', redirectTo: 'login'},
+
+  {
+    path: 'login',
+    loadComponent: () => import('./components/auth/login/login.component')
+      .then(m => m.LoginComponent) },
+
+  // Ruta que redigirá al dashboard de la aplicación
+
+  {
+    path: 'login',
+    loadComponent: () => import('./components/base/dashboard/dashboard.component')
+      .then(m => m.DashboardComponent)
+  },
+
+];
