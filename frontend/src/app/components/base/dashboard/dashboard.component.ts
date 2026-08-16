6888747e78143260e2bdca58332aac842f8e1516
Adrián Vicente Vera <adrianvicentevera04@gmail.com>
2026-08-16 14:15:05 +0200

    Diseño dashboard component + métodos para gastoService.

--- frontend/src/app/components/base/dashboard/dashboard.component.ts
+++ frontend/src/app/components/base/dashboard/dashboard.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Injectable, inject } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
-}
+  // Inyección de dependencias
+
+
+
+} // class
--- frontend/src/app/services/gasto/gasto.service.ts
+++ frontend/src/app/services/gasto/gasto.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Gasto } from '../../models/Gasto/Gasto';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GastoService {
+  // Inyección de dependencias
 
-  constructor() { }
-}
+  private readonly http = inject(HttpClient);
+
+  // Declaración de variables
+
+  private readonly apiUrl: string = 'https://localhost:8080/api/gastos';
+  constructor() {}
+
+  // Método para obtener todos los gastos
+
+  public obtenerGastos(): Observable<Gasto[]> {
+    return this.http.get<Gasto[]>(`${this.apiUrl}`);
+
+  }
+
+} // class
